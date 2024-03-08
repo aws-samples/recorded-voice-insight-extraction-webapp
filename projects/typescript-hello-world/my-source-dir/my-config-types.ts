7b73d5b44453b0136830dfc018e5cb98e32b4697
David Kaleko <kaleko@amazon.com>
2024-03-08 19:58:18 +0000

    Demonstrating optional and default values in typescript config yaml reader

--- projects/typescript-hello-world/my-source-dir/my-config-types.ts
+++ projects/typescript-hello-world/my-source-dir/my-config-types.ts
@@ -4,4 +4,15 @@ export interface ConfigData {
         var1: string;
         var2: string[];
     }
+    var3?: string; // Optional variable
 }
+
+
+// Set default values for ConfigData
+export const defaultConfigData: ConfigData = {
+  block1: {
+    var1: 'defaultVar1',
+    var2: ['defaultItem1', 'defaultItem2'],
+  },
+  var3: 'let you down', // Default value for var3
+};
--- projects/typescript-hello-world/my-source-dir/test-read-config.ts
+++ projects/typescript-hello-world/my-source-dir/test-read-config.ts
@@ -1,11 +1,18 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import * as path from 'path';
-import { ConfigData } from './my-config-types';
+import { ConfigData, defaultConfigData } from './my-config-types';
 
 // Cfg file always one directory up from this typescript file
 const cfgPath: string = path.resolve(__dirname, '..', 'config.yml');
 const fileContents = fs.readFileSync(cfgPath, 'utf8');
-const data: ConfigData = yaml.load(fileContents) as ConfigData;
+const userConfig: ConfigData = yaml.load(fileContents) as ConfigData;
 
-console.log(data.block1.var2);
+// Merge userConfig with default values
+const mergedConfig: ConfigData = {
+    ...defaultConfigData,
+    ...userConfig,
+};
+
+console.log(mergedConfig.block1.var2)
+console.log(mergedConfig.var3)
