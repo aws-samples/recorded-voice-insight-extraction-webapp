bdfa547202d9283f3aff882e4ae0fefff8c53ffc
David Kaleko <kaleko@amazon.com>
2024-09-24 20:00:10 +0000

    Better default name for stack

--- infra/app.py
+++ infra/app.py
@@ -12,7 +12,7 @@ ReVIEWStack(
     # Stack names should be very short (max 14 characters)
     # Stack names should be unique, as s3 buckets named after it must be globally unique.
     # Note: the cognito user-pool name is hard coded and shared by all stacks for now.
-    "ReVIEW-dev-339712833620",
+    "ReVIEW-dev-your-acct-num-here",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
