d30320b2c65f421c04f34a45213fe5cd4d4b4cf2
David Kaleko <kaleko@amazon.com>
2024-10-22 01:10:51 +0000

    This function is deprecated

--- frontend/components/s3_utils.py
+++ frontend/components/s3_utils.py
@@ -38,15 +38,6 @@ def retrieve_transcript_by_jobid(job_id: str, username: str) -> str:
     )
 
 
-def retrieve_transcript_json_by_jobid(job_id: str, username: str) -> dict:
-    """Read transcript json from s3 and return"""
-    return json.load(
-        s3.get_object(Bucket=BUCKET_NAME, Key=f"transcripts/{username}/{job_id}.json")[
-            "Body"
-        ]
-    )
-
-
 def retrieve_media_bytes(media_name: str, username: str) -> bytes:
     """Read media from s3 and return"""
     return s3.get_object(Bucket=BUCKET_NAME, Key=f"recordings/{username}/{media_name}")[
