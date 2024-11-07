4062575edadaf39785b65926d832bc5515f7ec5f
David Kaleko <kaleko@amazon.com>
2024-11-07 19:35:59 +0000

    Removing zip file from source control, replacing with PythonLayerVersion and alpha release of cdk package

--- infra/constructs/oss_constructs.py
+++ infra/constructs/oss_constructs.py
@@ -232,23 +232,11 @@ class ReVIEWOSSConstruct(Construct):
         1. Create the lambda function that will create an index
         2. Invoke the lambda function to actually create the index (w/ custom resource stuff)
         """
-        # dependency layer (includes requests, requests-aws4auth,opensearch-py, aws-lambda-powertools)
-        # dependency_layer = _lambda.LayerVersion(
-        #     self,
-        #     "dependency_layer",
-        #     code=_lambda.Code.from_asset("assets/oss_lambdas_dependency_layer.zip"),
-        #     compatible_runtimes=[
-        #         _lambda.Runtime.PYTHON_3_8,
-        #         _lambda.Runtime.PYTHON_3_9,
-        #         _lambda.Runtime.PYTHON_3_10,
-        #     ],
-        #     license="Apache-2.0",
-        #     description="dependency_layer including requests, requests-aws4auth, aws-lambda-powertools, opensearch-py",
-        # )
+
         dependency_layer = PythonLayerVersion(
             self,
             "dependency_layer",
-            entry="assets",  # directory containing requirements.txt for lambda dependency layer
+            entry="lambda-layers",  # directory containing requirements.txt for lambda dependency layer
             compatible_runtimes=[
                 _lambda.Runtime.PYTHON_3_8,
                 _lambda.Runtime.PYTHON_3_9,
